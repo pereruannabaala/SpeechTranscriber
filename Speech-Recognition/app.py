1ddb2a444884761cda61550bd6112e8f48ed4e8e
pereruannabaala <pereruannabaala@gmail.com>
2024-09-24 18:17:50 +0300

    import redirect

--- Speech-Recognition/app.py
+++ Speech-Recognition/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         print("FORM DATA RECEIVED")
+    
+    if "file" not in request.files:
+        return redirect(request.url)
+    
+    
     return render_template('index.html')
 
 if __name__ == '__main__':
