84b94d0cf70623a03281fa834de7f0e71baf38a6
pereruannabaala <pereruannabaala@gmail.com>
2024-09-24 18:20:46 +0300

    add instance where there's file safe

--- Speech-Recognition/app.py
+++ Speech-Recognition/app.py
@@ -7,10 +7,12 @@ def index():
     if request.method == 'POST':
         print("FORM DATA RECEIVED")
     
-    if "file" not in request.files:
-        return redirect(request.url)
+        if "file" not in request.files:
+            return redirect(request.url)
     
-    
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
     return render_template('index.html')
 
 if __name__ == '__main__':
